96861e36852cedd492c6bce2f37c0ea25c3366f2
ogarnd <aspirinasaucer@gmail.com>
2024-07-18 00:23:47 -0300

    * Fixed typo.

--- llc_append_html.h
+++ llc_append_html.h
@@ -12,7 +12,7 @@ namespace llc
 	llc::err_t	appendHtmlTableRow	(::llc::achar & output, ::llc::vcs tagAttributes = {}, ::llc::vcs innerHtml = {});
 	llc::err_t	appendHtmlTableCol	(::llc::achar & output, ::llc::vcs tagAttributes = {}, ::llc::vcs innerHtml = {});
 	llc::err_t	appendHtmlPage		(::llc::achar & output, const ::llc::FAppend & funcAppendCSS, const ::llc::FAppend & funcAppendJS, const ::llc::FAppend & funcAppendBody);
-	llc::err_t	appendHtmlPage		(::llc::achar & output, const ::llc::FAppend & funcAppendHead, const ::llc::FAppend & funcAppendBody)	
+	llc::err_t	appendHtmlPage		(::llc::achar & output, const ::llc::FAppend & funcAppendHead, const ::llc::FAppend & funcAppendBody);
 	llc::err_t	appendHtmlPage		(::llc::achar & output, const ::llc::FAppend & funcAppendBody);
 } // namespace 
 
