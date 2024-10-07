96d007ba7613d6f315ac19704bf065de2515cff2
ogarnd <aspirinasaucer@gmail.com>
2024-10-07 16:54:17 -0300

    * Formatting fixes.

--- llc_timer.cpp
+++ llc_timer.cpp
@@ -11,23 +11,23 @@ stxp	double			MICROSECOND_SCALE		= 0.000001;
 
 void					llc::STimer::Reset		()				noexcept				{
 	LastTimeSeconds			= double(LastTimeMicroseconds = 0);
-#if defined(LLC_WINDOWS) || defined(LLC_CMSIS) 
+#if !(defined(LLC_WINDOWS) || defined(LLC_CMSIS))
+#	ifdef LLC_ARDUINO
+	PrevTimeStamp			= micros();
+#	else // !LLC_ARDUINO
+	PrevTimeStamp			= ::std::chrono::high_resolution_clock::now();
+#	endif // LLC_ARDUINO
+#else // (LLC_WINDOWS || LLC_CMSIS)
 #	ifdef LLC_WINDOWS
 	QueryPerformanceFrequency((::LARGE_INTEGER*)&CountsPerSecond);
 	QueryPerformanceCounter((::LARGE_INTEGER*)&PrevTimeStamp);
 #	elif defined(LLC_CMSIS)
 	CountsPerSecond			= osKernelGetTickFreq();
 	PrevTimeStamp			= osKernelGetTickCount();
-#	endif
+#	endif // LLC_WINDOWS
 	SecondsPerCount			= 1.0 / CountsPerSecond;
 	MicrosecondsPerCount	= 1.0 / (CountsPerSecond * MICROSECOND_SCALE);
-#else
-#	ifdef LLC_ARDUINO
-	PrevTimeStamp			= micros();
-#	else
-	PrevTimeStamp			= ::std::chrono::high_resolution_clock::now();
-#	endif
-#endif
+#endif // !(LLC_WINDOWS || LLC_CMSIS)
 }
 
 double			llc::STimer::Frame		()				noexcept				{
@@ -37,20 +37,20 @@ double			llc::STimer::Frame		()				noexcept				{
 	QueryPerformanceCounter((::LARGE_INTEGER*)&currentTimeStamp);
 #	elif defined(LLC_CMSIS)
 	int64_t				currentTimeStamp		= osKernelGetTickCount();
-#	endif
+#	endif // LLC_WINDOWS
 	LastTimeSeconds			= (currentTimeStamp - PrevTimeStamp) * SecondsPerCount;
 	LastTimeMicroseconds	= uint64_t((currentTimeStamp - PrevTimeStamp) * MicrosecondsPerCount);
 	PrevTimeStamp			= currentTimeStamp;
 	return LastTimeSeconds;
-#else
+#else // !(LLC_WINDOWS || LLC_CMSIS) 
 #	ifdef LLC_ARDUINO
 	int64_t		currentTimeStamp	= (int64_t)micros();
 	LastTimeMicroseconds	= currentTimeStamp - PrevTimeStamp;
-#	else
+#	else // !LLC_ARDUINO
 	auto		currentTimeStamp		= ::std::chrono::high_resolution_clock::now();
 	LastTimeMicroseconds	= (int64_t)::std::chrono::duration_cast<std::chrono::microseconds>(currentTimeStamp - PrevTimeStamp).count();
-#	endif
+#	endif // LLC_ARDUINO
 	PrevTimeStamp			= currentTimeStamp;
 	return LastTimeSeconds	= LastTimeMicroseconds * MICROSECOND_SCALE;
-#endif
+#endif // LLC_WINDOWS || LLC_CMSIS
 }
